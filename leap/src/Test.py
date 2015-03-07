c5f683bfb74d8426a01346215e8a96bd84217760
Shawn Kothari <dkothari777@gmail.com>
2015-03-06 22:47:13 -0700

    Starting Leap HelloWorld tutorial

---
+++ leap/src/Test.py
@@ -0,0 +1,27 @@
+_author_ = 'Darshan Kothari'
+
+import os, sys, inspect
+src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+lib_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
+sys.path.insert(0, lib_dir)
+import Leap
+
+
+def main():
+
+        print "Press Enter to quit ..."
+        try:
+            sys.stdin.readline()
+        except KeyboardInterrupt:
+            pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
